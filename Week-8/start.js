dbc79476d00d4e1188a9269a8b299f21b253e1c2
Harshit Patel <harshit3228@gmail.com>
2022-08-11 13:05:51 +0530

    Exercise8.5

--- Week-8/start.js
+++ Week-8/start.js
@@ -4,4 +4,6 @@ require("babel-register")({
 
 //module.exports = require("./Exercise8.1/Exercise8.1.js");
 //module.exports = require("./Exercise8.2/Exercise8.2.js");
-module.exports = require("./Exercise8.3/Exercise8.3.js");
+//module.exports = require("./Exercise8.3/Exercise8.3.js");
+//module.exports = require("./Exercise8.4/Exercise8.4.js");
+module.exports = require("./Exercise8.5/Exercise8.5.js");
