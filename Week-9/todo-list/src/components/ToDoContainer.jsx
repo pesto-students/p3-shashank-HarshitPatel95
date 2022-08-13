b073c1d0ccb6e749c2c1b6ac5eb38481f21a7160
Harshit Patel <harshit3228@gmail.com>
2022-08-13 12:43:57 +0530

    create todo header

---
+++ Week-9/todo-list/src/components/ToDoContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ToDoHeader from "./ToDoHeader";
+
+const ToDoContainer = () => {
+  return (
+    <div className="todo-div">
+      <ToDoHeader />
+    </div>
+  );
+};
+
+export default ToDoContainer;
---
+++ Week-9/todo-list/src/components/ToDoHeader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ToDoHeader = () => {
+  return (
+    <div className="todo-header">
+      <h1> TO-DO LIST </h1>
+    </div>
+  );
+};
+
+export default ToDoHeader;
