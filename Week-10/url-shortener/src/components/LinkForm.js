5e4ba1d41e2d5bfea07d6635c575526f21650d56
Harshit Patel <harshit3228@gmail.com>
2022-08-29 23:57:22 +0530

    add api call for url shortener

--- Week-10/url-shortener/src/components/LinkForm.js
+++ Week-10/url-shortener/src/components/LinkForm.js
@@ -1,13 +1,33 @@
+import React, { useState } from "react";
+
 const LinkForm = () => {
+  const [enteredUrl, setEnteredUrl] = useState("");
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    fetch("https://api.shrtco.de/v2/shorten?url=" + enteredUrl)
+      .then((res) => res.json())
+      .then(
+        (res) => {
+          console.log(res.result.full_short_link);
+          setEnteredUrl("");
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
     return false;
   };
 
   return (
     <div className="linkform-container">
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Shorten your link" />
+        <input
+          type="text"
+          placeholder="Shorten your link"
+          value={enteredUrl}
+          onChange={(e) => setEnteredUrl(e.target.value)}
+        />
         <button>Shorten</button>
       </form>
     </div>
