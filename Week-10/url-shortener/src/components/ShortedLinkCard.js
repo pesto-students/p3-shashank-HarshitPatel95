e725fe125c2c98cdbe441c28e22fe967f0f413c4
Harshit Patel <harshit3228@gmail.com>
2022-08-30 01:39:42 +0530

    copy button in working and final commit

--- Week-10/url-shortener/src/components/ShortedLinkCard.js
+++ Week-10/url-shortener/src/components/ShortedLinkCard.js
@@ -3,7 +3,10 @@ const ShortedLinkCard = (props) => {
     <div className="shorted-link-card">
       <p>{props.longurl}</p>
       <p>{props.shorturl}</p>
-      <i className="las la-clipboard"></i>
+      <i
+        onClick={() => navigator.clipboard.writeText(props.shorturl)}
+        className="las la-clipboard"
+      ></i>
     </div>
   );
 };
